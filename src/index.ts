571c12675dd0b58f37fe10d1b2585d97e02a0d33
Alexander O'Mara <me@alexomara.com>
2024-08-27 20:53:08 -0400

    Added index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './types.ts';
 export * from './const.ts';
 export * from './memoryfile.ts';
 export * from './blob.ts';
-export * from './blob/codedirectory.ts';
+export * from './blob/index.ts';
