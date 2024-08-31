2be33b56c8518cb46b9f455a2ffbbba92188bb05
Alexander O'Mara <me@alexomara.com>
2024-08-31 18:46:36 -0400

    Better test

--- src/util.test.ts
+++ src/util.test.ts
@@ -5,14 +5,14 @@ import {stringToBytes} from './util.ts';
 
 void describe('util', () => {
 	void describe('stringToBytes', () => {
-		void it('foo', () => {
-			const b = stringToBytes('foo');
-			deepStrictEqual(b, new Uint8Array([0x66, 0x6f, 0x6f]));
+		void it('abc', () => {
+			const b = stringToBytes('abc');
+			deepStrictEqual(b, new Uint8Array([0x61, 0x62, 0x63]));
 		});
 
-		void it('foo null', () => {
-			const b = stringToBytes('foo\0');
-			deepStrictEqual(b, new Uint8Array([0x66, 0x6f, 0x6f, 0x00]));
+		void it('abc null', () => {
+			const b = stringToBytes('abc\0');
+			deepStrictEqual(b, new Uint8Array([0x61, 0x62, 0x63, 0x00]));
 		});
 
 		void it('U+00A3', () => {
