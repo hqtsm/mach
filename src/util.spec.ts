60446d46c0a034d1bce0ca085597f26805352c49
Alexander O'Mara <me@alexomara.com>
2024-09-03 20:57:55 -0400

    Added util to extract macho arch

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,6 +1,19 @@
 import {open} from 'node:fs/promises';
 import {inflateRaw} from 'node:zlib';
 
+import {BufferView} from './type.ts';
+import {
+	FAT_CIGAM,
+	FAT_CIGAM_64,
+	FAT_MAGIC,
+	FAT_MAGIC_64,
+	MH_CIGAM,
+	MH_CIGAM_64,
+	MH_MAGIC,
+	MH_MAGIC_64
+} from './const.ts';
+import {subview} from './util.ts';
+
 async function zlibInflateRaw(data: Buffer) {
 	return new Promise<Buffer>((resolve, reject) => {
 		inflateRaw(data, (err, data) => {
@@ -147,4 +160,91 @@ export async function fixtureMacho(
 		throw new Error(`Missing files: ${[...m.keys()].join(' ')}`);
 	}
 	return datas;
+}
+
+export function machoArch(
+	data: BufferView,
+	type: number,
+	subtype: number | null = null
+) {
+	const v = subview(DataView, data);
+	const magic = v.getUint32(0);
+	let f = false;
+	let b = false;
+	let e = false;
+	switch (magic) {
+		case MH_MAGIC: {
+			break;
+		}
+		case MH_MAGIC_64: {
+			b = true;
+			break;
+		}
+		case MH_CIGAM: {
+			e = true;
+			break;
+		}
+		case MH_CIGAM_64: {
+			e = true;
+			b = true;
+			break;
+		}
+		case FAT_MAGIC: {
+			f = true;
+			break;
+		}
+		case FAT_MAGIC_64: {
+			f = true;
+			b = true;
+			break;
+		}
+		case FAT_CIGAM: {
+			f = true;
+			e = true;
+			break;
+		}
+		case FAT_CIGAM_64: {
+			f = true;
+			e = true;
+			b = true;
+			break;
+		}
+		default: {
+			throw new Error(`Unknown magic: ${magic}`);
+		}
+	}
+	if (!f) {
+		const cputype = v.getInt32(4, e);
+		const cpusubtype = v.getInt32(8, e);
+		if (cputype !== type || (subtype && cpusubtype !== subtype)) {
+			throw new Error(`Wrong arch: ${cputype} ${cpusubtype}`);
+		}
+		return subview(Uint8Array, v);
+	}
+	const count = v.getUint32(4, b);
+	const slices: [number, number][] = [];
+	let o = 8;
+	for (let i = 0; i < count; i++) {
+		const cputype = v.getInt32(o, e);
+		o += 4;
+		const cpusubtype = v.getInt32(o, e);
+		o += 4;
+		const offset = b ? Number(v.getBigUint64(o, e)) : v.getUint32(o, e);
+		o += b ? 8 : 4;
+		const size = b ? Number(v.getBigUint64(o, e)) : v.getUint32(o, e);
+		o += b ? 8 : 4;
+		// Ignore align:
+		o += 4;
+		if (cputype === type && (subtype === null || cpusubtype === subtype)) {
+			slices.push([offset, size]);
+		}
+	}
+	if (slices.length < 1) {
+		throw new Error('No matching arch');
+	}
+	if (slices.length > 1) {
+		throw new Error('Multiple matching archs');
+	}
+	const [[offset, size]] = slices;
+	return subview(Uint8Array, data, offset, size);
 }
