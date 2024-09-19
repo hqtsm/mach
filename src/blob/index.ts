a7a55196fa36b5c114c5f3d8d62929715bad9338
Alexander O'Mara <me@alexomara.com>
2024-09-18 21:39:57 -0400

    Removed obsolete

--- src/blob/index.ts
+++ src/blob/index.ts
@@ -1,6 +1,5 @@
 export * from './blobwrapper.ts';
 export * from './codedirectory.ts';
-export * from './entitlementblob.ts';
 export * from './entitlementderblob.ts';
 export * from './launchconstraintblob.ts';
 export * from './requirements.ts';
