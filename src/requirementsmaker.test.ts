5c9ce1a89e681c856e695c9c5c0dd00ece001cb4
Alexander O'Mara <me@alexomara.com>
2024-09-29 00:51:45 -0400

    Shorter var

--- src/requirementsmaker.test.ts
+++ src/requirementsmaker.test.ts
@@ -20,12 +20,12 @@ void describe('requirementsmaker', () => {
 
 	void it('host + designated', () => {
 		// host => anchor apple and identifier com.apple.host
-		const hostData = unhex(
+		const host = unhex(
 			'00 00 00 01 00 00 00 02 00 00 00 0E',
 			'63 6F 6D 2E 61 70 70 6C 65 2E 68 6F 73 74 00 00'
 		);
 		// designated => anchor apple and identifier com.apple.designated
-		const designatedData = unhex(
+		const designated = unhex(
 			'00 00 00 01 00 00 00 02 00 00 00 14',
 			'63 6F 6D 2E 61 70 70 6C 65 2E 64 65 73 69 67 6E 61 74 65 64'
 		);
@@ -35,16 +35,13 @@ void describe('requirementsmaker', () => {
 			'00 00 00 01 00 00 00 1C',
 			'00 00 00 03 00 00 00 40',
 			'FA DE 0C 00 00 00 00 24',
-			Buffer.from(hostData).toString('hex'),
+			Buffer.from(host).toString('hex'),
 			'FA DE 0C 00 00 00 00 28',
-			Buffer.from(designatedData).toString('hex')
+			Buffer.from(designated).toString('hex')
 		);
 		const rsm = new RequirementsMaker();
-		rsm.add(kSecHostRequirementType, Requirement.blobify(hostData));
-		rsm.add(
-			kSecDesignatedRequirementType,
-			Requirement.blobify(designatedData)
-		);
+		rsm.add(kSecHostRequirementType, Requirement.blobify(host));
+		rsm.add(kSecDesignatedRequirementType, Requirement.blobify(designated));
 		const rs = rsm.make();
 		deepStrictEqual(
 			new Uint8Array(rs.buffer, rs.byteOffset, rs.byteLength),
