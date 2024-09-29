af4e294464c26ea94164d19a380897c2e23ce405
Alexander O'Mara <me@alexomara.com>
2024-09-29 02:38:23 -0400

    Test RequirementMaker builder methods

--- src/requirementmaker.test.ts
+++ src/requirementmaker.test.ts
@@ -2,9 +2,10 @@ import {describe, it} from 'node:test';
 import {deepStrictEqual, strictEqual} from 'node:assert';
 
 import {Requirement} from './requirement.ts';
-import {kSecCodeMagicRequirement} from './const.ts';
+import {kSecCodeMagicRequirement, opAnd, opOr} from './const.ts';
 import {unhex} from './util.spec.ts';
 import {RequirementMaker} from './requirementmaker.ts';
+import {RequirementMakerChain} from './requirementmakerchain.ts';
 
 function fibinacci(n: number) {
 	const fib = [1, 1];
@@ -63,5 +64,165 @@ void describe('requirementmaker', () => {
 		strictEqual(dv.getUint32(0), kSecCodeMagicRequirement);
 		strictEqual(dv.getUint32(4), r.byteLength);
 		strictEqual(dv.getUint32(8), Requirement.lwcrForm);
+	});
+
+	void it('identifier "com.apple.simple"', () => {
+		const data = unhex(
+			'FA DE 0C 00 00 00 00 24 00 00 00 01',
+			'00 00 00 02',
+			'00 00 00 10',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 73 69 6D 70 6C 65'
+		);
+		const maker = new RequirementMaker(Requirement.exprForm);
+		maker.ident(new TextEncoder().encode('com.apple.simple'));
+		const r = maker.make();
+		deepStrictEqual(
+			new Uint8Array(r.buffer, r.byteOffset, r.byteLength),
+			data
+		);
+	});
+
+	void it('anchor apple and identifier "com.apple.simple"', () => {
+		const data = unhex(
+			'FA DE 0C 00 00 00 00 2C 00 00 00 01',
+			'00 00 00 06',
+			'00 00 00 03',
+			'00 00 00 02',
+			'00 00 00 10',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 73 69 6D 70 6C 65'
+		);
+		const maker = new RequirementMaker(Requirement.exprForm);
+
+		const and = RequirementMakerChain.newRequirementMakerChain(
+			maker,
+			opAnd
+		);
+		maker.anchor();
+		and.add();
+		maker.ident(new TextEncoder().encode('com.apple.simple'));
+		and.add();
+
+		const r = maker.make();
+		deepStrictEqual(
+			new Uint8Array(r.buffer, r.byteOffset, r.byteLength),
+			data
+		);
+	});
+
+	void it('identifier "com.apple.simple" or anchor apple generic', () => {
+		const data = unhex(
+			'FA DE 0C 00 00 00 00 2C 00 00 00 01',
+			'00 00 00 07',
+			'00 00 00 02',
+			'00 00 00 10',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 73 69 6D 70 6C 65',
+			'00 00 00 0F'
+		);
+		const maker = new RequirementMaker(Requirement.exprForm);
+
+		const or = RequirementMakerChain.newRequirementMakerChain(maker, opOr);
+		maker.ident(new TextEncoder().encode('com.apple.simple'));
+		or.add();
+		maker.anchorGeneric();
+		or.add();
+
+		const r = maker.make();
+		deepStrictEqual(
+			new Uint8Array(r.buffer, r.byteOffset, r.byteLength),
+			data
+		);
+	});
+
+	void it('(a and b) or (c and d)', () => {
+		// identifier "com.apple.app" and anchor apple or
+		// identifier "com.apple.gen" and anchor apple generic
+		const data = unhex(
+			'FA DE 0C 00 00 00 00 50 00 00 00 01',
+			'00 00 00 07',
+			'00 00 00 06',
+			'00 00 00 02',
+			'00 00 00 0D',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 61 70 70 00 00 00',
+			'00 00 00 03',
+			'00 00 00 06',
+			'00 00 00 02',
+			'00 00 00 0D',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 67 65 6E 00 00 00',
+			'00 00 00 0F'
+		);
+		const maker = new RequirementMaker(Requirement.exprForm);
+
+		const or = RequirementMakerChain.newRequirementMakerChain(maker, opOr);
+		let and;
+
+		and = RequirementMakerChain.newRequirementMakerChain(maker, opAnd);
+		maker.ident(new TextEncoder().encode('com.apple.app'));
+		and.add();
+		maker.anchor();
+		and.add();
+
+		or.add();
+
+		and = RequirementMakerChain.newRequirementMakerChain(maker, opAnd);
+		maker.ident(new TextEncoder().encode('com.apple.gen'));
+		and.add();
+		maker.anchorGeneric();
+		and.add();
+
+		or.add();
+
+		const r = maker.make();
+		deepStrictEqual(
+			new Uint8Array(r.buffer, r.byteOffset, r.byteLength),
+			data
+		);
+	});
+
+	void it('(a or b) and (c or d)', () => {
+		// (identifier "com.apple.app" or anchor apple) and
+		// (identifier "com.apple.gen" or anchor apple generic)
+		const data = unhex(
+			'FA DE 0C 00 00 00 00 50 00 00 00 01',
+			'00 00 00 06',
+			'00 00 00 07',
+			'00 00 00 02',
+			'00 00 00 0D',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 61 70 70 00 00 00',
+			'00 00 00 03',
+			'00 00 00 07',
+			'00 00 00 02',
+			'00 00 00 0D',
+			'63 6F 6D 2E 61 70 70 6C 65 2E 67 65 6E 00 00 00',
+			'00 00 00 0F'
+		);
+		const maker = new RequirementMaker(Requirement.exprForm);
+
+		const and = RequirementMakerChain.newRequirementMakerChain(
+			maker,
+			opAnd
+		);
+		let or;
+
+		or = RequirementMakerChain.newRequirementMakerChain(maker, opOr);
+		maker.ident(new TextEncoder().encode('com.apple.app'));
+		or.add();
+		maker.anchor();
+		or.add();
+
+		and.add();
+
+		or = RequirementMakerChain.newRequirementMakerChain(maker, opOr);
+		maker.ident(new TextEncoder().encode('com.apple.gen'));
+		or.add();
+		maker.anchorGeneric();
+		or.add();
+
+		and.add();
+
+		const r = maker.make();
+		deepStrictEqual(
+			new Uint8Array(r.buffer, r.byteOffset, r.byteLength),
+			data
+		);
 	});
 });
