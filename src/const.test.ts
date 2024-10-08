7b510aec7a56aa4cec015fd8f5a2fa40df7e24c8
Alexander O'Mara <me@alexomara.com>
2024-10-07 23:11:58 -0400

    Test constants for the correct sign

---
+++ src/const.test.ts
@@ -0,0 +1,23 @@
+import {describe, it} from 'node:test';
+
+import * as constants from './const.ts';
+
+const signed = new Set([
+	'BIND_SPECIAL_DYLIB_FLAT_LOOKUP',
+	'BIND_SPECIAL_DYLIB_MAIN_EXECUTABLE',
+	'BIND_SPECIAL_DYLIB_WEAK_LOOKUP',
+	'CPU_TYPE_ANY',
+	'CPU_SUBTYPE_ANY',
+	'CPU_SUBTYPE_MULTIPLE'
+]);
+
+void describe('const', () => {
+	void it('sign', () => {
+		for (const k of Object.keys(constants)) {
+			const v = (constants as {[k: string]: unknown})[k];
+			if (typeof v === 'number' && v < 0 && !signed.has(k)) {
+				throw new Error(`Invalid constant: ${k}`);
+			}
+		}
+	});
+});
