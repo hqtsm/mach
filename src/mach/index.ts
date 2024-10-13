0b4f0b8c39d487b3001ce740ef66eec9f0b293a3
Alexander O'Mara <me@alexomara.com>
2024-10-13 00:12:01 -0400

    UuidCommand struct

--- src/mach/index.ts
+++ src/mach/index.ts
@@ -29,3 +29,4 @@ export * from './dylibreference.ts';
 export * from './twolevelhintscommand.ts';
 export * from './twolevelhint.ts';
 export * from './prebindcksumcommand.ts';
+export * from './uuidcommand.ts';
