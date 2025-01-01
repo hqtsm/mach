dbeaa4e34ec1e3759eb238be574bf8d155b8ea44
Alexander O'Mara <me@alexomara.com>
2025-01-01 16:17:22 -0500

    SuperBlob find

--- blob/superblob.test.ts
+++ blob/superblob.test.ts
@@ -45,7 +45,7 @@ Deno.test('blob', () => {
 	);
 });
 
-Deno.test('blob', () => {
+Deno.test('find', () => {
 	const maker = new SuperBlobMaker();
 	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
