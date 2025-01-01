3fa978bebbf6e373b08a21643271317bf29b2513
Alexander O'Mara <me@alexomara.com>
2025-01-01 16:17:06 -0500

    SuperBlob find

--- blob/superblob.test.ts
+++ blob/superblob.test.ts
@@ -44,3 +44,26 @@ Deno.test('blob', () => {
 		new Uint8Array(blob2.buffer, 0, 16),
 	);
 });
+
+Deno.test('blob', () => {
+	const maker = new SuperBlobMaker();
+	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
+	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
+	const blob1 = BlobWrapper.alloc(data1);
+	const blob2 = BlobWrapper.alloc(data2);
+	maker.add(1, blob1);
+	maker.add(2, blob2);
+	const sb = maker.make();
+	const get1 = sb.find(1)!;
+	const get2 = sb.find(2)!;
+	const get3 = sb.find(3)!;
+	assertEquals(
+		new Uint8Array(get1.buffer, get1.byteOffset, get1.length),
+		new Uint8Array(blob1.buffer, 0, 16),
+	);
+	assertEquals(
+		new Uint8Array(get2.buffer, get2.byteOffset, get2.length),
+		new Uint8Array(blob2.buffer, 0, 16),
+	);
+	assertEquals(get3, null);
+});
