7e85aa3020be660a9665653124371b2812f49e18
Alexander O'Mara <me@alexomara.com>
2025-01-01 16:11:31 -0500

    SuperBlob blob

--- blob/superblob.test.ts
+++ blob/superblob.test.ts
@@ -17,3 +17,30 @@ Deno.test('type', () => {
 	assertEquals(sb.type(0), 0x01020304);
 	assertEquals(sb.type(1), 0x10203040);
 });
+
+Deno.test('blob', () => {
+	const maker = new SuperBlobMaker();
+	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
+	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
+	const blob1 = BlobWrapper.alloc(data1);
+	const blob2 = BlobWrapper.alloc(data2);
+	maker.add(1, blob1);
+	maker.add(2, blob2);
+	const sb = maker.make();
+	const get1 = sb.blob(0)!;
+	const get2 = sb.blob(1)!;
+
+	assertEquals(
+		new Uint8Array(get1.buffer, get1.byteOffset, get1.length),
+		new Uint8Array(blob1.buffer, 0, 16),
+	);
+
+	// No offset means null. What would create this?
+	new DataView(sb.buffer).setUint32(16, 0);
+	assertEquals(sb.blob(0), null);
+
+	assertEquals(
+		new Uint8Array(get2.buffer, get2.byteOffset, get2.length),
+		new Uint8Array(blob2.buffer, 0, 16),
+	);
+});
