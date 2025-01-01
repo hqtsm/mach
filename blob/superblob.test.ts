e0fc6d9b7def9cede83b77270dc549660dac4bc5
Alexander O'Mara <me@alexomara.com>
2025-01-01 15:55:04 -0500

    SuperBlob type

--- blob/superblob.test.ts
+++ blob/superblob.test.ts
@@ -1,6 +1,19 @@
 import { assertEquals } from '@std/assert';
 import { SuperBlob } from './superblob.ts';
+import { SuperBlobMaker } from './superblobmaker.ts';
+import { BlobWrapper } from './blobwrapper.ts';
 
 Deno.test('BYTE_LENGTH', () => {
 	assertEquals(SuperBlob.BYTE_LENGTH, 12);
+});
+
+Deno.test('type', () => {
+	const maker = new SuperBlobMaker();
+	const data = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
+	const blob = BlobWrapper.alloc(data);
+	maker.add(0x01020304, blob);
+	maker.add(0x10203040, blob);
+	const sb = maker.make();
+	assertEquals(sb.type(0), 0x01020304);
+	assertEquals(sb.type(1), 0x10203040);
 });
