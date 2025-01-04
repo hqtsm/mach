ee51c4afe5c8deaa0eab1ea38b242b3a8735f98f
Alexander O'Mara <me@alexomara.com>
2025-01-04 14:32:47 -0500

    Removed memory file, using blobs instead

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 export * from './blob/mod.ts';
 export * from './const.ts';
-export * from './file.ts';
 export * from './mach/mod.ts';
 export * from './macho/mod.ts';
 export * from './util/mod.ts';
