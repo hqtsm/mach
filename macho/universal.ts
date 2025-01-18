e7bf16fa4b0a15cbdd26314680a5c896caae0b96
Alexander O'Mara <me@alexomara.com>
2025-01-17 20:49:38 -0500

    Read header, byte swap

--- macho/universal.ts
+++ macho/universal.ts
@@ -1,3 +1,13 @@
+import {
+	FAT_CIGAM,
+	FAT_MAGIC,
+	MH_CIGAM,
+	MH_CIGAM_64,
+	MH_MAGIC,
+	MH_MAGIC_64,
+} from '../const.ts';
+import { FatHeader } from '../mach/fatheader.ts';
+import { MachHeader } from '../mach/machheader.ts';
 import type { Reader } from '../util/reader.ts';
 
 /**
@@ -45,5 +55,36 @@ export class Universal {
 		this.mLength = length;
 		this.mMachType = 0;
 		this.mSuspicious = false;
+
+		const hs = Math.max(FatHeader.BYTE_LENGTH, MachHeader.BYTE_LENGTH);
+		const hd = await reader.slice(offset, offset + hs).arrayBuffer();
+		if (hd.byteLength !== hs) {
+			throw new RangeError('Invalid header');
+		}
+
+		let header = new FatHeader(hd);
+		let mHeader;
+		switch (header.magic) {
+			case FAT_CIGAM:
+				header = new FatHeader(hd, 0, !header.littleEndian);
+				// falls through
+			case FAT_MAGIC: {
+				// TODO
+				break;
+			}
+			case MH_CIGAM:
+			case MH_CIGAM_64:
+				mHeader = new MachHeader(hd, 0, !header.littleEndian);
+				// falls through
+			case MH_MAGIC:
+			case MH_MAGIC_64: {
+				mHeader ??= new MachHeader(hd, 0);
+				// TODO
+				break;
+			}
+			default: {
+				throw new RangeError('Invalid header');
+			}
+		}
 	}
 }
