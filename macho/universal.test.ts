38dfa582116d431b420e3640f6a41b2a4125a09d
Alexander O'Mara <me@alexomara.com>
2025-01-18 22:22:10 -0500

    Simpler isSuspicious for binaries, later test offset and length separate

--- macho/universal.test.ts
+++ macho/universal.test.ts
@@ -5,7 +5,7 @@ import { Universal } from './universal.ts';
 
 const fixtures = fixtureMachos();
 
-for (const [index, { kind, arch, file, archs }] of fixtures.entries()) {
+for (const { kind, arch, file, archs } of fixtures) {
 	Deno.test(`${kind}: ${arch}: ${file}`, async () => {
 		const [macho] = await fixtureMacho(kind, arch, [file]);
 		const uni = new Universal();
@@ -15,41 +15,13 @@ for (const [index, { kind, arch, file, archs }] of fixtures.entries()) {
 		assertEquals(uni.isUniversal(), false);
 		assertEquals(uni.isSuspicious(), false);
 
-		switch (index % 4) {
-			case 0: {
-				const blob = new Blob([macho]);
-				await uni.open(blob);
-				assertEquals(uni.offset(), 0);
-				assertEquals(uni.length(), 0);
-				break;
-			}
-			case 1: {
-				const blob = new Blob([macho]);
-				await uni.open(blob, 0, blob.size);
-				assertEquals(uni.offset(), 0);
-				assertEquals(uni.length(), blob.size);
-				break;
-			}
-			case 2: {
-				const pad = new ArrayBuffer(3);
-				const blob = new Blob([pad, macho, pad]);
-				await uni.open(blob, pad.byteLength);
-				assertEquals(uni.offset(), pad.byteLength);
-				assertEquals(uni.length(), 0);
-				break;
-			}
-			case 3: {
-				const pad = new ArrayBuffer(3);
-				const blob = new Blob([pad, macho, pad]);
-				await uni.open(blob, pad.byteLength, macho.byteLength);
-				assertEquals(uni.offset(), pad.byteLength);
-				assertEquals(uni.length(), macho.byteLength);
-				break;
-			}
-		}
+		const blob = new Blob([macho]);
+		await uni.open(blob);
+		assertEquals(uni.offset(), 0);
+		assertEquals(uni.length(), 0);
 		assertEquals(uni.isOpen(), true);
 		assertEquals(uni.isUniversal(), archs.size > 1);
-		// assertEquals(uni.isSuspicious(), false);
+		assertEquals(uni.isSuspicious(), false);
 
 		const architectures = new Set<Architecture>();
 		uni.architectures(architectures);
