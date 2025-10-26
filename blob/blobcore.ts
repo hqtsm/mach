8c063368600a62f4b86a073b96dc5f94a97aaf9c
Alexander O'Mara <me@alexomara.com>
2025-10-26 12:56:16 -0400

    Made readonly

--- blob/blobcore.ts
+++ blob/blobcore.ts
@@ -193,7 +193,7 @@ export class BlobCore extends Struct {
 	 * @param BlobType Blob type.
 	 * @returns Is the same type.
 	 */
-	public is(BlobType: { typeMagic: number }): boolean {
+	public is(BlobType: Readonly<{ typeMagic: number }>): boolean {
 		return this.mMagic === BlobType.typeMagic;
 	}
 
