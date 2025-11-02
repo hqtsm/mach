46a87d143ab05de332772a4c4cfa5376aa06bb22
Alexander O'Mara <me@alexomara.com>
2025-11-02 00:49:06 -0400

    Correct extension

--- blob/embeddedsignatureblobmaker.ts
+++ blob/embeddedsignatureblobmaker.ts
@@ -1,10 +1,10 @@
 import { EmbeddedSignatureBlob } from './embeddedsignatureblob.ts';
-import { SuperBlobMaker } from './superblobmaker.ts';
+import { SuperBlobCoreMaker } from './superblobcoremaker.ts';
 
 /**
  * SuperBlob maker for EmbeddedSignatureBlob.
  */
-export class EmbeddedSignatureBlobMaker extends SuperBlobMaker {
+export class EmbeddedSignatureBlobMaker extends SuperBlobCoreMaker {
 	declare public readonly ['constructor']: Omit<
 		typeof EmbeddedSignatureBlobMaker,
 		'new'
