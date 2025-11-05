c17003caa220fddcb0fd59e708aa2944ef3a626b
Alexander O'Mara <me@alexomara.com>
2025-11-04 21:29:11 -0500

    Better SuperBlob testing strategy

--- blob/superblob.test.ts
+++ blob/superblob.test.ts
@@ -1,14 +1,33 @@
 import { assertEquals } from '@std/assert';
+import { constant } from '@hqtsm/struct';
 import { BlobWrapper } from './blobwrapper.ts';
 import { SuperBlob } from './superblob.ts';
 import { SuperBlobMaker } from './superblobmaker.ts';
+
+const MAGIC = 0x12345678;
+
+class Example extends SuperBlob {
+	declare public readonly ['constructor']: Omit<typeof Example, 'new'>;
+
+	public static override readonly typeMagic = MAGIC;
+
+	static {
+		constant(this, 'typeMagic');
+	}
+}
+
+class ExampleMaker extends SuperBlobMaker {
+	declare public readonly ['constructor']: Omit<typeof ExampleMaker, 'new'>;
+
+	public static override readonly SuperBlob = Example;
+}
 
 Deno.test('BYTE_LENGTH', () => {
 	assertEquals(SuperBlob.BYTE_LENGTH, 12);
 });
 
 Deno.test('type', () => {
-	const maker = new SuperBlobMaker();
+	const maker = new ExampleMaker();
 	const data = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 	const blob = BlobWrapper.alloc(data);
 	maker.add(0x01020304, blob);
@@ -19,7 +38,7 @@ Deno.test('type', () => {
 });
 
 Deno.test('blob', () => {
-	const maker = new SuperBlobMaker();
+	const maker = new ExampleMaker();
 	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
 	const blob1 = BlobWrapper.alloc(data1);
@@ -46,7 +65,7 @@ Deno.test('blob', () => {
 });
 
 Deno.test('find', () => {
-	const maker = new SuperBlobMaker();
+	const maker = new ExampleMaker();
 	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
 	const blob1 = BlobWrapper.alloc(data1);
@@ -69,7 +88,7 @@ Deno.test('find', () => {
 });
 
 Deno.test('count', () => {
-	const maker = new SuperBlobMaker();
+	const maker = new ExampleMaker();
 	const data1 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8]);
 	const data2 = new Uint8Array([2, 3, 4, 5, 6, 7, 8, 9]);
 	const blob1 = BlobWrapper.alloc(data1);
