fa70381af44c88e0cc6802e35c68eeb62ae12d21
Alexander O'Mara <me@alexomara.com>
2025-12-16 02:43:22 -0500

    Test just size argument

--- blob/blobwrapper.test.ts
+++ blob/blobwrapper.test.ts
@@ -42,7 +42,20 @@ Deno.test('alloc length', () => {
 	);
 });
 
-Deno.test('alloc view', () => {
+Deno.test('alloc size', () => {
+	const data = new Uint8Array(16);
+	const bw = BlobWrapper.alloc(data.byteLength);
+	const dv = new DataView(bw.buffer, bw.byteOffset, 8);
+	assertEquals(dv.getUint32(0), CSMAGIC_BLOBWRAPPER);
+	assertEquals(dv.getUint32(4), bw.length() + 8);
+	const ptr = bw.data();
+	assertEquals(
+		new Uint8Array(ptr.buffer, ptr.byteOffset, bw.length()),
+		data,
+	);
+});
+
+Deno.test('alloc buffer', () => {
 	const data = unhex('09 AB CD EF 01 02 03 04 05 06 07 08 09 0A 0B 0C');
 	const bw = BlobWrapper.alloc(data.buffer, data.byteLength);
 	const dv = new DataView(bw.buffer, bw.byteOffset, 8);
