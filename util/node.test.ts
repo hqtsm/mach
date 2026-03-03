3431cc87d164d70dc862e25394ad67117aefd204
Alexander O'Mara <me@alexomara.com>
2026-03-02 23:31:07 -0500

    Directly test hashCryptoFromNodeCrypto function

---
+++ util/node.test.ts
@@ -0,0 +1,31 @@
+// deno-lint-ignore no-external-import
+import { createHash } from 'node:crypto';
+import { assertEquals } from '@std/assert';
+import { hashCryptoFromNodeCrypto } from './node.ts';
+
+Deno.test('hashCryptoFromNodeCrypto', async () => {
+	const data = new Uint8Array(256);
+	for (let i = data.length; i--;) {
+		data[i] = i;
+	}
+
+	const nc = hashCryptoFromNodeCrypto({ createHash });
+	for (const algo of ['SHA-1', 'SHA-256', 'SHA-384', 'SHA-512'] as const) {
+		// deno-lint-ignore no-await-in-loop
+		const [expected, nodeAB, nodeAG] = await Promise.all([
+			crypto.subtle.digest(algo, data),
+			nc.digest(algo, data),
+			nc.digest(
+				algo,
+				(async function* (): AsyncGenerator<ArrayBuffer> {
+					yield data.slice(0, 128).buffer;
+					yield data.slice(128).buffer;
+				})(),
+			),
+		]);
+
+		const expect = new Uint8Array(expected);
+		assertEquals(new Uint8Array(nodeAB), expect, algo);
+		assertEquals(new Uint8Array(nodeAG), expect, algo);
+	}
+});
