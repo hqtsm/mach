b1c021e6ce059a143cd07baf1c5221804778725e
Alexander O'Mara <me@alexomara.com>
2026-03-15 01:35:17 -0400

    Test cleanup

--- security/mod.test.ts
+++ security/mod.test.ts
@@ -3,28 +3,27 @@ import { Blob } from './blob.ts';
 import { SuperBlobMaker } from './superblobmaker.ts';
 import * as mod from './mod.ts';
 
+// deno-lint-ignore ban-types
+function isSubclass<T extends Function>(Type: T, value: unknown): value is T {
+	return value instanceof Function && value.prototype instanceof Type;
+}
+
 Deno.test('Blob typeMagic', () => {
 	for (const [name, value] of Object.entries(mod)) {
 		if (
-			!(value instanceof Function) ||
-			!(value.prototype instanceof Blob) ||
-			name === 'SuperBlob' ||
-			name === 'SuperBlobCore'
+			isSubclass(Blob, value) &&
+			name !== 'SuperBlob' &&
+			name !== 'SuperBlobCore'
 		) {
-			continue;
+			assert(Object.hasOwn(value, 'typeMagic'), name);
 		}
-		assert(Object.hasOwn(value, 'typeMagic'), name);
 	}
 });
 
 Deno.test('SuperBlobMaker SuperBlob', () => {
 	for (const [name, value] of Object.entries(mod)) {
-		if (
-			!(value instanceof Function) ||
-			!(value.prototype instanceof SuperBlobMaker)
-		) {
-			continue;
+		if (isSubclass(SuperBlobMaker, value)) {
+			assert(Object.hasOwn(value, 'SuperBlob'), name);
 		}
-		assert(Object.hasOwn(value, 'SuperBlob'), name);
 	}
 });
