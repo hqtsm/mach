751eeb4f67feaa6f826fe06a1bfa0cbff61598e7
Alexander O'Mara <me@alexomara.com>
2026-03-15 01:24:06 -0400

    Simpler tests

--- blob/mod.test.ts
+++ blob/mod.test.ts
@@ -3,33 +3,28 @@ import { Blob } from './blob.ts';
 import { SuperBlobMaker } from './superblobmaker.ts';
 import * as mod from './mod.ts';
 
-Deno.test('typeMagic', () => {
-	for (const name of Object.keys(mod)) {
-		if (name === 'SuperBlob' || name === 'SuperBlobCore') {
+Deno.test('Blob typeMagic', () => {
+	for (const [name, value] of Object.entries(mod)) {
+		if (
+			!(value instanceof Function) ||
+			!(value.prototype instanceof Blob) ||
+			name === 'SuperBlob' ||
+			name === 'SuperBlobCore'
+		) {
 			continue;
 		}
-		const all = mod as {
-			[name: string]: typeof Blob | unknown;
-		};
-		const Bl = all[name] as typeof Blob | undefined;
-		if (Bl && Bl.prototype && Bl.prototype instanceof Blob) {
-			assert(Object.hasOwn(Bl, 'typeMagic'), name);
-		}
+		assert(Object.hasOwn(value, 'typeMagic'), name);
 	}
 });
 
-Deno.test('SuperBlob', () => {
-	for (const name of Object.keys(mod)) {
-		const all = mod as {
-			[name: string]: typeof SuperBlobMaker | unknown;
-		};
-		const Maker = all[name] as typeof SuperBlobMaker | undefined;
+Deno.test('SuperBlobMaker SuperBlob', () => {
+	for (const [name, value] of Object.entries(mod)) {
 		if (
-			Maker &&
-			Maker.prototype &&
-			Maker.prototype instanceof SuperBlobMaker
+			!(value instanceof Function) ||
+			!(value.prototype instanceof SuperBlobMaker)
 		) {
-			assert(Object.hasOwn(Maker, 'SuperBlob'), name);
+			continue;
 		}
+		assert(Object.hasOwn(value, 'SuperBlob'), name);
 	}
 });
