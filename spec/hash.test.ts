41684f53ca6a5b3c37abe52ed59ce12a9445661d
Alexander O'Mara <me@alexomara.com>
2026-03-21 16:22:14 -0400

    More tests

--- spec/hash.test.ts
+++ spec/hash.test.ts
@@ -1,23 +1,27 @@
 import { assertEquals } from '@std/assert';
 import { BadReader } from './hash.ts';
 
-Deno.test('BadReader', () => {
+Deno.test('BadReader', async () => {
 	const bad = new BadReader(10, 'application/test');
 	assertEquals(bad.type, 'application/test');
 
 	bad.diff = 1;
 	assertEquals(bad.diff, 1);
 	assertEquals(bad.size, 10);
+	assertEquals((await bad.arrayBuffer()).byteLength, 9);
 
 	bad.diff = 2;
 	assertEquals(bad.diff, 2);
 	assertEquals(bad.size, 10);
+	assertEquals((await bad.arrayBuffer()).byteLength, 8);
 
 	bad.diff = -1;
 	assertEquals(bad.diff, -1);
 	assertEquals(bad.size, 10);
+	assertEquals((await bad.arrayBuffer()).byteLength, 11);
 
 	bad.diff = -2;
 	assertEquals(bad.diff, -2);
 	assertEquals(bad.size, 10);
+	assertEquals((await bad.arrayBuffer()).byteLength, 12);
 });
