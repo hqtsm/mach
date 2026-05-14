a9022491bd2d8723486d0c14d5f73181ebb9b857
Alexander O'Mara <me@alexomara.com>
2026-05-14 00:45:59 -0400

    Optional subtle

--- sec/SecCertificate.ts
+++ sec/SecCertificate.ts
@@ -1,6 +1,7 @@
 import { toStringTag } from '@hqtsm/class';
 import { INT32_MAX } from '../libc/stdint.ts';
 import type { SecCertificateRef } from '../Security/SecBase.ts';
+import type { SubtleCryptoDigest } from '../util/crypto.ts';
 import { SecSHA1DigestCreate, SecSHA256DigestCreate } from './SecDigest.ts';
 
 /**
@@ -21,10 +22,12 @@ export class __SecCertificate {
  * Get SHA-1 digest for certificate.
  *
  * @param certificate Certificate.
+ * @param subtle Hash crypto.
  * @returns Digest.
  */
 export async function SecCertificateCopySHA1Digest(
 	certificate: SecCertificateRef | null,
+	subtle?: SubtleCryptoDigest | null,
 ): Promise<null | ArrayBuffer> {
 	let der;
 	if (
@@ -34,17 +37,23 @@ export async function SecCertificateCopySHA1Digest(
 	) {
 		return null;
 	}
-	return await SecSHA1DigestCreate(new Uint8Array(der), der.byteLength);
+	return await SecSHA1DigestCreate(
+		new Uint8Array(der),
+		der.byteLength,
+		subtle,
+	);
 }
 
 /**
  * Get SHA-256 digest for certificate.
  *
  * @param certificate Certificate.
+ * @param subtle Hash crypto.
  * @returns Digest.
  */
 export async function SecCertificateCopyIssuerSHA256Digest(
 	certificate: SecCertificateRef | null,
+	subtle?: SubtleCryptoDigest | null,
 ): Promise<null | ArrayBuffer> {
 	let der;
 	if (
@@ -54,5 +63,9 @@ export async function SecCertificateCopyIssuerSHA256Digest(
 	) {
 		return null;
 	}
-	return await SecSHA256DigestCreate(new Uint8Array(der), der.byteLength);
+	return await SecSHA256DigestCreate(
+		new Uint8Array(der),
+		der.byteLength,
+		subtle,
+	);
 }
