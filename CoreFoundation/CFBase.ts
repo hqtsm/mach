a556e1a37fdfea03ed3a579218c2c4fd7c3f907b
Alexander O'Mara <me@alexomara.com>
2026-06-18 00:09:35 -0400

    Moved types

--- CoreFoundation/CFBase.ts
+++ CoreFoundation/CFBase.ts
@@ -1,10 +1,20 @@
-import type { PLNull } from '@hqtsm/plist';
+import type { PLNull, PLString } from '@hqtsm/plist';
 import type { size_t } from '../libc/stddef.ts';
 
 /**
  * CF index.
  */
 export type CFIndex = size_t;
+
+/**
+ * CF string.
+ */
+export type CFStringRef = PLString;
+
+/**
+ * CF mutable string.
+ */
+export type CFMutableStringRef = PLString;
 
 /**
  * CF null.
